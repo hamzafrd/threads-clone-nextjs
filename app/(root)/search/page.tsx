68db0274e1949695e5a9186135a41ddc85dd78be
Hamza Firdaus <elhamzafirdaus@gmail.com>
2023-11-11 13:24:34 +0700

    Add search section

---
+++ app/(root)/search/page.tsx
@@ -0,0 +1,54 @@
+import UsersCard from "@/components/cards/UsersCard";
+import { Iuser } from "@/interface";
+import { fetchAllUser, fetchUser } from "@/lib/actions/user.action";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+const Search = async () => {
+	const user = await currentUser();
+
+	if (!user) return null;
+
+	//get id user from other user
+	const userInfo: Iuser = await fetchUser(user.id);
+	if (!userInfo?.onboarded) redirect("/on-boarding");
+
+	//fetch all other users
+	const result = await fetchAllUser({
+		userId: user.id,
+		searchString: "",
+		pageNumber: 1,
+		pageSize: 25,
+	});
+
+	return (
+		<section>
+			<h1 className="head-text mb-10">Search</h1>
+
+			{/* Search Bar */}
+
+			<div className="mt-14 flex flex-col gap-9">
+				{result.users.length === 0 ? (
+					<p className="no-result">No Users</p>
+				) : (
+					<>
+						{result.users.map((person) => {
+							return (
+								<UsersCard
+									key={person.id}
+									id={person.id}
+									name={person.name}
+									username={person.username}
+									imgUrl={person.image}
+									personType={"user"}
+								/>
+							);
+						})}
+					</>
+				)}
+			</div>
+		</section>
+	);
+};
+
+export default Search;
---
+++ components/cards/UsersCard.tsx
@@ -0,0 +1,44 @@
+"use client";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { Button } from "../ui/button";
+
+type Params = {
+	key: string;
+	id: string;
+	name: string;
+	username: string;
+	imgUrl: string;
+	personType: string;
+};
+
+const UsersCard = ({ id, name, username, imgUrl, personType }: Params) => {
+	const router = useRouter();
+	return (
+		<article className="user-card">
+			<div className="user-card_avatar">
+				<Image
+					src={imgUrl}
+					alt="logo"
+					width={48}
+					height={48}
+					className="rounded-full"
+				/>
+
+				<div className="flex-1 text-ellipsis">
+					<h4 className="text-base-semibold text-light-1">{name}</h4>
+					<p className="text-small-medium text-gray-1">{username}</p>
+				</div>
+
+				<Button
+					className="user-card_btn"
+					onClick={() => router.push(`/profile/${id}`)}
+				>
+					View
+				</Button>
+			</div>
+		</article>
+	);
+};
+
+export default UsersCard;
